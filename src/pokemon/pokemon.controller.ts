7069b6c1ad97a4cca1028c4e1368e6a5aaadc3b2
Emerson Tanno <tannobrizotto@gmail.com>
2025-04-04 00:06:11 -0300

    Commit inicial

---
+++ src/pokemon/pokemon.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { PokemonService } from './pokemon.service';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+
+@Controller('pokemon')
+export class PokemonController {
+  constructor(private readonly service: PokemonService) {}
+
+  @Post()
+  create(@Body() dto: CreatePokemonDto) {
+    return this.service.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.service.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.service.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdatePokemonDto) {
+    return this.service.update(+id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.service.remove(+id);
+  }
+}
---
+++ src/pokemon/pokemon.service.spec.ts
@@ -0,0 +1,41 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
+
+@Injectable()
+export class PokemonService {
+  private pokemons: Pokemon[] = [];
+  private idCounter = 1;
+
+  create(dto: CreatePokemonDto): Pokemon {
+    const pokemon: Pokemon = {
+      id: this.idCounter++,
+      ...dto,
+    };
+    this.pokemons.push(pokemon);
+    return pokemon;
+  }
+
+  findAll(): Pokemon[] {
+    return this.pokemons;
+  }
+
+  findOne(id: number): Pokemon {
+    const found = this.pokemons.find((p) => p.id === id);
+    if (!found) throw new NotFoundException('Pokémon not found');
+    return found;
+  }
+
+  update(id: number, dto: UpdatePokemonDto): Pokemon {
+    const pokemon = this.findOne(id);
+    Object.assign(pokemon, dto);
+    return pokemon;
+  }
+
+  remove(id: number): void {
+    const index = this.pokemons.findIndex((p) => p.id === id);
+    if (index === -1) throw new NotFoundException('Pokémon not found');
+    this.pokemons.splice(index, 1);
+  }
+}
